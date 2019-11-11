f9ca6b7ae23d07505afcefd27b48ad9804be2901
iAriel <ariel@aluno.fapce.edu.br>
2019-11-11 17:08:56 -0300

    Lista Usando Java

---
+++ src/Model/Produto.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Ariel
+ */
+public class Produto {
+    private String marca;
+    private String produto;
+    private String qtd;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public String getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(String qtd) {
+        this.qtd = qtd;
+    }
+    
+    
+}
